473fb55bcd3a27b8572ae26ec79a4fdfc2d257bc
johnwaithira <waithirajon@gmail.com>
2024-02-22 12:13:52 +0300

    TodoApp

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Task from "./components/Task";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hello React</Text>
-      <StatusBar style="auto" />
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's Task</Text>
+        <View style={styles.items}>
+          <Task></Task>
+          <Task text={'Task 1'}></Task>
+          <Task text={'Task 2'}></Task>
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,12 +20,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#EBEAED",
   },
   title: {
-    fontSize :24,
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  tasksWrapper: {
+    paddingTop:80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
     fontWeight:'bold',
   },
+  items: {},
 });
